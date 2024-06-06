093279a5137685fc30cfdb9aa742491a132df81c
a23i127 <a23i127ne@ous.jp>
2024-06-06 20:30:15 +0900

    ２回目です

---
+++ plactices.swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  plactices.swift
+//
+//  Created by 高橋沙久哉 on 2024/06/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ plactices.swift/newContolorarViewController.swift
@@ -0,0 +1,29 @@
+//
+//  newContolorarViewController.swift
+//  plactices.swift
+//
+//  Created by 高橋沙久哉 on 2024/06/06.
+//
+
+import UIKit
+
+class newContolorarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
